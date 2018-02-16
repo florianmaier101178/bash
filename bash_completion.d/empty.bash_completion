9d44d38990d04f07f0dc37ef959657204a75cf7b
Florian Maier <florian.maier@daimler.com>
2018-02-16 10:13:38 +0100

    added empty files for correct working bashrc.d and bash_completion.d

---
+++ bash_completion.d/empty.bash_completion
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#is needed that .bashrc works
--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@
 # create .bashrc.d
 if [ ! -d ~/.bashrc.d ]; then
     mkdir -p ~/.bashrc.d
+    cp ./bashrc.d/empty.bashrc ~/.bashrc.d/
 fi
 
 # create .bash_completion.d
 if [ ! -d ~/bash_completion.d ]; then
     mkdir -p ~/.bash_completion.d
+    cp ./bash_completion.d/empty.bash_completion ~/.bash_completion.d/
 fi
 
 # backup .bashrc if present
